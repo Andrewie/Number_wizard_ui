754aa8fda68b8d8482feb8ce45f34b54758c0c25
Andrewie <andreassundstr03m@gmail.com>
2018-10-28 11:23:24 +0100

    Initial commit

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] Text guessText;
+    int guess;
+    int[] guesses;
+    int guessNr;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    // Use this for initialization
+
+    public void OnPressHigher()
+    {
+        min = guess + 1;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess-1;
+        NextGuess();
+    }
+
+    public void OnPressQuit()
+    {
+        Application.Quit();
+
+    }
+
+
+
+
+    void StartGame()
+    {
+        NextGuess();
+        guessText.text = guess.ToString();
+    }
+
+    void NextGuess()
+    {
+
+        guess = Random.Range(min, max+1);
+        guessText.text = guess.ToString();
+    }
+
+}
